691b0f84ccf76751e38ced153eb0f83248a012c3
JoLeeSoftwire <Jo.Lee@softwire.com>
2020-04-06 15:37:45 +0100

    crossword: wip add db connection and simple model (untested)

---
+++ src/main/java/com/thursday/crossword/model/db/Clue.java
@@ -0,0 +1,14 @@
+package com.thursday.crossword.model.db;
+
+
+public class Clue {
+    private int id;
+    private String text;
+    private String answer;
+
+    public int getId() { return id; }
+
+    public String getText() { return text; }
+
+    public String getAnswer() { return answer; }
+}
