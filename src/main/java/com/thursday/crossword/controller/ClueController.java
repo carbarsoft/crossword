1f0aa728ddd9dc3c9df45250048098a2d097962a
CarBar <carolyn.barker@softwire.com>
2020-04-27 17:18:28 +0100

    WIP

--- src/main/java/com/thursday/crossword/controller/ClueController.java
+++ src/main/java/com/thursday/crossword/controller/ClueController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("clues")
 public class ClueController {
@@ -26,8 +28,20 @@ public class ClueController {
     public @ResponseBody Clue postClue(String text, String answer) {
         Clue clue = new Clue();
         clue.setText(text);
-        clue.setAnswer(answer);
+        clue.setAnswer(answer.toLowerCase());
         cluesRepository.save(clue);
         return clue;
+    }
+
+    @GetMapping("/search")
+    public @ResponseBody List<Clue> searchAnswers(@RequestParam String searchString) {
+        searchString.toLowerCase();
+        String regex = convertToRegex(searchString);
+        List<Clue> results = cluesRepository.answerMatcher(regex);
+        return results;
+    }
+
+    private String convertToRegex(String searchString) {
+        return "%";
     }
 }
--- src/main/java/com/thursday/crossword/repository/CluesRepository.java
+++ src/main/java/com/thursday/crossword/repository/CluesRepository.java
@@ -2,8 +2,12 @@ package com.thursday.crossword.repository;
 
 
 import com.thursday.crossword.model.db.Clue;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CluesRepository extends CrudRepository<Clue, Integer> {
+import java.util.List;
 
+public interface CluesRepository extends CrudRepository<Clue, Integer> {
+    @Query(value="SELECT * FROM clue")
+    public List<Clue> answerMatcher(String likeString);
 }
